587edadd98706d5aacc9ef83aacb8d9880e21f3e
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-07-14 00:08:18 -0700

    📱 set up media breakpoints and queries

--- style/theme.js
+++ style/theme.js
@@ -1,13 +1,37 @@
+import { css } from "styled-components";
+
+const colors = {
+  white: "#FFFFFF",
+  black: "#04080F",
+  dark: "#0B2027",
+  yellow: "#EAD637",
+  blue: "#507DBC"
+};
+
+const font = {
+  family: `'Literata', serif`
+};
+
+const breakpoints = {
+  tablet: "768px",
+  phone: "425px"
+};
+
 const theme = {
-  colors: {
-    white: "#FFFFFF",
-    black: "#04080F",
-    dark: "#0B2027",
-    yellow: "#EAD637",
-    blue: "#507DBC"
-  },
-  font: {
-    family: `'Literata', serif`
+  colors,
+  font,
+  breakpoints,
+  media: {
+    tablet: (...args) => css`
+      @media screen and (max-width: ${breakpoints.tablet}) {
+        ${css(...args)};
+      }
+    `,
+    phone: (...args) => css`
+      @media screen and (max-width: ${breakpoints.phone}) {
+        ${css(...args)};
+      }
+    `
   }
 };
 
