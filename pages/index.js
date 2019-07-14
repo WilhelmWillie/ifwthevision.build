6ee18807173d1304e79fcd1e870188a39027508b
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-07-14 00:18:28 -0700

    🦶 Added footer links to landing page

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,41 @@ export default () => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Landing />
+    <Footer>
+      <FooterLink
+        href="//twitter.com/wilhelm_willie"
+        target="_blank"
+        rel="noopener"
+      >
+        @wilhelm_willie
+      </FooterLink>
+      <FooterLink
+        href="http://wilhelmwillie.com"
+        target="_blank"
+        rel="noopener"
+      >
+        wilhelmwillie.com
+      </FooterLink>
+    </Footer>
   </Wrapper>
 );
 
 const Wrapper = styled.div`
   display: flex;
-  padding: 90px 0;
+  padding: 90px 25px;
+`;
+
+const Footer = styled.div`
+  position: fixed;
+  bottom: 50px;
+  left: 0;
+  width: 100%;
+  text-align: center;
+`;
+
+const FooterLink = styled.a`
+  color: ${p => p.theme.colors.white};
+  padding: 0 8px;
+  opacity: 0.3;
+  text-decoration: none;
 `;
