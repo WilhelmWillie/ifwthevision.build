32d189fcd4f62a0eb10b430ee7754e7ec735da9e
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-07-15 00:35:30 -0700

    📨 Added e-mail message that shows up when e-mail is submitted

--- components/EmailInput.js
+++ components/EmailInput.js
@@ -1,24 +1,37 @@
-import { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
+
+const SUCCESS_MESSAGE = `Your e-mail has been added to our e-mail list`;
 
 export default () => {
   const emailInput = useRef(null);
+  const [submittedEmail, setSubmittedEmail] = useState(false);
 
   const onButtonClick = () => {
     // TODO: Log e-mail to firebase
     const email = emailInput.current.value;
     console.log(`[EMAIL] Add ${email} to landing page email list`);
 
+    setSubmittedEmail(true);
+
     // Clear input
     emailInput.current.value = "";
   };
 
   return (
-    <InputContainer>
-      <Input type="email" placeholder="your@email.com" ref={emailInput} />
+    <React.Fragment>
+      <InputContainer>
+        <Input type="email" placeholder="your@email.com" ref={emailInput} />
 
-      <Button onClick={onButtonClick}>build</Button>
-    </InputContainer>
+        <Button onClick={onButtonClick}>build</Button>
+      </InputContainer>
+
+      {submittedEmail && (
+        <SuccessMessage>
+          <p>{SUCCESS_MESSAGE}</p>
+        </SuccessMessage>
+      )}
+    </React.Fragment>
   );
 };
 
@@ -50,3 +63,5 @@ const Button = styled.button`
   font-size: 16px;
   padding: 16px 24px;
 `;
+
+const SuccessMessage = styled.div``;
